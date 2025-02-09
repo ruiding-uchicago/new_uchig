a5ab9003f75ba1d93c39f49ff63c6bc1bb1f4568
Rui Ding <66197530+ruiding-uchicago@users.noreply.github.com>
2025-02-09 22:34:31 +0000

    try to add two https in settings.py

--- myportal/settings.py
+++ myportal/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_TITLE = 'MADE-PUBLIC Data Portal'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
 
 # SECURITY WARNING: keep all secret keys used in production secret!
 # You can generate a secure secret key with `openssl rand -hex 32`
